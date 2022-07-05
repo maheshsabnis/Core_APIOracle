29984c6363da35412ef518d23f919c4a0f0cb066
maheshsabnis <sabnis_m@hotmail.com>
2022-07-05 13:13:51 +0530

    Add project files.

---
+++ Core_APIOracle/Controllers/ProductInfoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Core_APIOracle.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core_APIOracle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductInfoController : ControllerBase
+    {
+        private readonly OraDbContext _context;
+
+        public ProductInfoController(OraDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _context.Products.ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(ProductsInfo productsInfo)
+        {
+            var result = await _context.Products.AddAsync(productsInfo);
+            await _context.SaveChangesAsync();
+            return Ok(result.Entity);
+        }
+    }
+}
---
+++ myconn.sql
@@ -0,0 +1,9 @@
+Create Table Department(
+  DeptNo integer Primary Key,
+  DeptName Varchar2(100) Not Null,
+  Location Varchar2(100) Not Null,
+  Capacity integer Not Null
+)
+
+
+select * from department
